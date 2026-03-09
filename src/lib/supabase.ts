967651c34a5b9e40912d10b6464676993406983e
cativy999 <ivoryivy333@gmail.com>
2026-03-09 14:35:45 -0700

    save current state

--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-// lib/supabase.ts
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://hueujkujdwkcfraxjwoq.supabase.co'
-const supabaseKey = 'sb_publishable_vTFfdy9ozV6xfpUc5HT3DA_XgsbaXjJ'
-export const supabase = createClient(supabaseUrl, supabaseKey)
